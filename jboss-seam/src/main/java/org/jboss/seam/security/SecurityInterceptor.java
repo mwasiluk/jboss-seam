b2878a076d6ec7f27fd514fa81d9233424d499ad
Mateusz Kamiński <m.kaminski@sages.com.pl>
2025-07-17 11:53:32 +0200

    Get rid of synchronized in SecurityContext

--- jboss-seam/src/main/java/org/jboss/seam/security/SecurityInterceptor.java
+++ jboss-seam/src/main/java/org/jboss/seam/security/SecurityInterceptor.java
@@ -7,6 +7,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.jboss.seam.annotations.intercept.AroundInvoke;
 import org.jboss.seam.annotations.intercept.Interceptor;
@@ -34,8 +35,8 @@ public class SecurityInterceptor extends AbstractInterceptor implements Serializ
     * You may encounter a JVM bug where the field initializer is not evaluated for a transient field after deserialization.
     * @see "http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=6252102"
     */
-   private transient volatile Map<Method,Restriction> restrictions = new HashMap<Method,Restriction>();
-   
+   private transient volatile ConcurrentHashMap<Method, Restriction> restrictions = new ConcurrentHashMap<Method, Restriction>();
+
    private class Restriction
    {
       private String expression;
@@ -163,121 +164,81 @@ public class SecurityInterceptor extends AbstractInterceptor implements Serializ
       return invocation.proceed();
    }
 
-   private Restriction getRestriction(Method interfaceMethod) throws Exception
-   {
-      // see field declaration as to why this is done
-      if (restrictions == null)
-      {
-         synchronized(this)
-         {
-            restrictions = new HashMap<Method, Restriction>();
-         }
-      }
-      
-      if (!restrictions.containsKey(interfaceMethod))
-      {
-         synchronized(restrictions)
-         {
-            // FIXME this logic should be abstracted rather than sitting in the middle of this interceptor
-            if (!restrictions.containsKey(interfaceMethod))
-            {  
-               Restriction restriction = null;
-               
-               Method method = getComponent().getBeanClass().getMethod( 
-                     interfaceMethod.getName(), interfaceMethod.getParameterTypes() );      
-               
-               Restrict restrict = null;
-               
-               if ( method.isAnnotationPresent(Restrict.class) )
-               {
-                  restrict = method.getAnnotation(Restrict.class);
+   private Restriction getRestriction(Method interfaceMethod) throws Exception {
+       Restriction restriction = restrictions.get(interfaceMethod);
+       if (restriction != null) {
+           return restriction;
+       }
+
+       Method method = getComponent().getBeanClass().getMethod(
+           interfaceMethod.getName(), interfaceMethod.getParameterTypes());
+
+       Restrict restrict = null;
+       if (method.isAnnotationPresent(Restrict.class)) {
+           restrict = method.getAnnotation(Restrict.class);
+       } else if (getComponent().getBeanClass().isAnnotationPresent(Restrict.class)) {
+           if (!getComponent().isLifecycleMethod(method)) {
+               restrict = getComponent().getBeanClass().getAnnotation(Restrict.class);
+           }
+       }
+
+       Restriction newRestriction = null;
+       if (restrict != null) {
+           newRestriction = new Restriction();
+           if (Strings.isEmpty(restrict.value())) {
+               newRestriction.setPermissionTarget(getComponent().getName());
+               newRestriction.setPermissionAction(method.getName());
+           } else {
+               newRestriction.setExpression(restrict.value());
+           }
+       }
+
+       for (Annotation annotation : method.getDeclaringClass().getAnnotations()) {
+           if (annotation.annotationType().isAnnotationPresent(RoleCheck.class)) {
+               if (newRestriction == null) newRestriction = new Restriction();
+               newRestriction.addRoleRestriction(annotation.annotationType().getSimpleName().toLowerCase());
+           }
+       }
+
+       for (Annotation annotation : method.getAnnotations()) {
+           if (annotation.annotationType().isAnnotationPresent(PermissionCheck.class)) {
+               PermissionCheck permissionCheck = annotation.annotationType().getAnnotation(PermissionCheck.class);
+
+               Method valueMethod = null;
+               for (Method m : annotation.annotationType().getDeclaredMethods()) {
+                   valueMethod = m;
+                   break;
                }
-               else if ( getComponent().getBeanClass().isAnnotationPresent(Restrict.class) )
-               {
-                  if ( !getComponent().isLifecycleMethod(method) )
-                  {
-                     restrict = getComponent().getBeanClass().getAnnotation(Restrict.class); 
-                  }
+
+               if (valueMethod != null) {
+                   if (newRestriction == null) newRestriction = new Restriction();
+                   Object target = valueMethod.invoke(annotation);
+                   if (!target.equals(void.class)) {
+                       newRestriction.addMethodRestriction(target,
+                           getPermissionAction(permissionCheck, annotation));
+                   }
                }
-               
-               if (restrict != null)
-               {
-                  if (restriction == null) restriction = new Restriction();
-                  
-                  if ( Strings.isEmpty(restrict.value()) )
-                  {
-                     restriction.setPermissionTarget(getComponent().getName());
-                     restriction.setPermissionAction(method.getName());
-                  }
-                  else
-                  {
-                     restriction.setExpression(restrict.value());
-                  }
+           }
+           if (annotation.annotationType().isAnnotationPresent(RoleCheck.class)) {
+               if (newRestriction == null) newRestriction = new Restriction();
+               newRestriction.addRoleRestriction(annotation.annotationType().getSimpleName().toLowerCase());
+           }
+       }
+
+       for (int i = 0; i < method.getParameterAnnotations().length; i++) {
+           Annotation[] annotations = method.getParameterAnnotations()[i];
+           for (Annotation annotation : annotations) {
+               if (annotation.annotationType().isAnnotationPresent(PermissionCheck.class)) {
+                   PermissionCheck permissionCheck = annotation.annotationType().getAnnotation(PermissionCheck.class);
+                   if (newRestriction == null) newRestriction = new Restriction();
+                   newRestriction.addParameterRestriction(i,
+                       getPermissionAction(permissionCheck, annotation));
                }
-               
-               for (Annotation annotation : method.getDeclaringClass().getAnnotations())
-               {
-                  if (annotation.annotationType().isAnnotationPresent(RoleCheck.class))
-                  {
-                     if (restriction == null) restriction = new Restriction();
-                     restriction.addRoleRestriction(annotation.annotationType().getSimpleName().toLowerCase());
-                  }
-               }
-               
-               for (Annotation annotation : method.getAnnotations())
-               {
-                  if (annotation.annotationType().isAnnotationPresent(PermissionCheck.class))
-                  {
-                     PermissionCheck permissionCheck = annotation.annotationType().getAnnotation(
-                           PermissionCheck.class);
-                     
-                     Method valueMethod = null;
-                     for (Method m : annotation.annotationType().getDeclaredMethods())
-                     {
-                        valueMethod = m;
-                        break;
-                     }
-                     
-                     if (valueMethod != null)
-                     {                        
-                        if (restriction == null) restriction = new Restriction();
-                        Object target = valueMethod.invoke(annotation);
-                        if (!target.equals(void.class))
-                        {
-                           restriction.addMethodRestriction(target, 
-                                 getPermissionAction(permissionCheck, annotation));
-                        }
-                     }
-                  }
-                  if (annotation.annotationType().isAnnotationPresent(RoleCheck.class))
-                  {
-                     if (restriction == null) restriction = new Restriction();
-                     restriction.addRoleRestriction(annotation.annotationType().getSimpleName().toLowerCase());
-                  }
-               }               
-               
-               for (int i = 0; i < method.getParameterAnnotations().length; i++)
-               {
-                  Annotation[] annotations = method.getParameterAnnotations()[i]; 
-                  for (Annotation annotation : annotations)
-                  {
-                     if (annotation.annotationType().isAnnotationPresent(PermissionCheck.class))
-                     {                        
-                        PermissionCheck permissionCheck = annotation.annotationType().getAnnotation(
-                              PermissionCheck.class);
-                        if (restriction == null) restriction = new Restriction();
-                        restriction.addParameterRestriction(i, 
-                              getPermissionAction(permissionCheck, annotation));                        
-                     }
-                  }
-               }                             
-               
-               restrictions.put(interfaceMethod, restriction);
-               return restriction;
-            }
-         }
-      }
-      return restrictions.get(interfaceMethod);      
+           }
+       }
+
+       Restriction existing = restrictions.putIfAbsent(interfaceMethod, newRestriction);
+       return (existing != null) ? existing : newRestriction;
    }
    
    private String getPermissionAction(PermissionCheck check, Annotation annotation)
